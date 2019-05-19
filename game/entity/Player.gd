b273d4e1cb1e7883b49373ef2e2a7da236077784
Aezart Rothenus <heroofhyla@gmail.com>
2019-05-18 19:58:57 -0700

    Win and loss states

--- game/entity/Player.gd
+++ game/entity/Player.gd
@@ -12,7 +12,9 @@ var motion_this_frame = null
 var move_delta = Vector2(0,0)
 var move_direction = Vector2(0,0)
 var move_this_turn = Vector2(0,0)
+var dead = false
 onready var bomb_count_field = get_node("/root/Game/AdjacentBombCount")
+onready var banner_label = get_node("/root/Game/Banner")
 var input_mapping = {
 	"move_left": Vector2(-1,0),
 	"move_right": Vector2(1,0),
@@ -25,11 +27,13 @@ var input_keys = input_mapping.keys()
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func _physics_process(delta):
+	if dead:
+		if Input.is_action_just_pressed("ui_accept"):
+			get_tree().reload_current_scene()
+			return
+		return
+	
 	input_motion = zero
 	for input in input_keys:
 		if Input.is_action_pressed(input):
@@ -57,10 +61,7 @@ func _physics_process(delta):
 					move_delta.y = 0
 	else:
 		if adjacent_bomb_count == -1:
-			adjacent_bomb_count = 0
-			for area in $BombDetectArea.get_overlapping_areas():
-				if area.is_in_group("Bomb"):
-					adjacent_bomb_count += 1
+			adjacent_bomb_count = len($DetectArea.get_overlapping_areas())
 			bomb_count_field.text = str(adjacent_bomb_count)
 
 		if input_motion != zero:
@@ -70,3 +71,27 @@ func _physics_process(delta):
 			bomb_count_field.text = "?"
 			moving = true;
 	
+
+func _on_TouchArea_area_entered(area):
+	print_debug(area)
+	if area.is_in_group("Bomb"):
+		area.trigger()
+		$DeathTimer.start(.3)
+	elif area.is_in_group("Goal"):
+		$WinTimer.start(.5)
+		
+
+
+func _on_DeathTimer_timeout():
+	banner_label.visible = true
+	banner_label.text = "GAME OVER\nPress ENTER to retry"
+	print_debug("YOU DIED")
+	dead = true
+	$Sprite.visible = false
+
+func _on_WinTimer_timeout():
+	dead = true
+	banner_label.visible = true
+	banner_label.text = "YOU WIN!\nPress ENTER to continue"
+	for bomb in get_tree().get_nodes_in_group("Bomb"):
+		bomb.reveal()
